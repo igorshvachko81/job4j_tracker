c3a6c3ba9c1c29048b636be7f7b355acef7a7348
Igor <ussr981@gmail.com>
2021-10-14 17:58:42 +0300

    6. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item localTime = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        LocalDateTime ldt = localTime.getCreated();
+        String dt = ldt.format(formatter);
+        System.out.println(dt);
+    }
+}
