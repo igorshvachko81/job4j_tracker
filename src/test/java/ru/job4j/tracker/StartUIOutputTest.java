11395da8a1313141ab0a57a1f53fc0d3fac73e5f
Igor <ussr981@gmail.com>
2021-11-02 22:10:56 +0200

    9.2. Тесты вывода на консоль [#33585 #223509]

--- src/test/java/ru/job4j/tracker/StartUIOutputTest.java
+++ src/test/java/ru/job4j/tracker/StartUIOutputTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+import org.w3c.dom.ls.LSOutput;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
@@ -53,6 +55,7 @@ public class StartUIOutputTest {
                         + "0. Show all Items" + ln
                         + "1. Exit Program" + ln
                         + "=== Show all items ===" + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Show all Items" + ln
                         + "1. Exit Program" + ln
@@ -79,6 +82,7 @@ public class StartUIOutputTest {
                         + "0. Find item by name" + ln
                         + "1. Exit Program" + ln
                         + "=== Find items by name ===" + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Find item by name" + ln
                         + "1. Exit Program" + ln
@@ -105,8 +109,63 @@ public class StartUIOutputTest {
                         + "0. Find item by ID" + ln
                         + "1. Exit Program" + ln
                         + "=== Find item by id ===" + ln
+                        + one + ln
                         + "Menu:" + ln
                         + "0. Find item by ID" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+        ));
+    }
+
+    @Test
+    public void whenCreateItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input in = new StubInput(
+                new String[] {"0", "test1", "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new CreateAction(out),
+                new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        Item one = tracker.findByName("test1")[0];
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Add new Item" + ln
+                        + "1. Exit Program" + ln
+                        + "==== Create a new Item ====" + ln
+                        + "Добавлена заявка: " + one + ln
+                        + "Menu:" + ln
+                        + "0. Add new Item" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Exit Program ===" + ln
+        ));
+    }
+
+    @Test
+    public void whenDeleteItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(one.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new DeleteItem(out),
+                new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                        + "0. Delete item" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Delete item ===" + ln
+                        + "Заявка удалена успешно." + ln
+                        + "Menu:" + ln
+                        + "0. Delete item" + ln
                         + "1. Exit Program" + ln
                         + "=== Exit Program ===" + ln
         ));
