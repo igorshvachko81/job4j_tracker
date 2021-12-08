025368cbe4cb1f22e926bbf71c36fd7a1415c5ad
Igor <ussr981@gmail.com>
2021-12-08 22:17:09 +0200

    1. Телефонный справочник на базе ArrayList [#41598 #236891]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -19,11 +19,27 @@ public class PhoneDictionaryTest {
         phones.add(
                 new Person("Ivan", "Arsentev", "534873", "Bryansk")
         );
-
         System.out.println("Search keyword is: \"Ars\"");
         ArrayList<Person> persons = phones.find("Ars");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
         assertThat(persons.get(1).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFindCompare() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Jonh", "Rembo", "82045078", "Smalltown")
+        );
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Ivan", "Arsentev", "534873", "Bryansk")
+        );
+        System.out.println("Search keyword is: \"Maestro\"");
+        ArrayList<Person> persons = phones.find("Maestro");
+        assertThat(persons.isEmpty(), is(true));
     }
 
 }
