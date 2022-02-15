60ea3a75ea5523e107c15aac3c8c99a5381bb0a6
Igor <ussr981@gmail.com>
2022-02-15 22:38:43 +0200

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,30 +2,75 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель данных банковского счёта
+ * @author Igor Shvachko
+ * @version 1.0
+ */
 public class Account {
+    /**
+     * Данные: идентификатор счёта(requisite)
+     * и баланс счёта(balance)
+     */
     private String requisite;
     private double balance;
+
+    /**
+     * Создание нового объекта класса конструктором
+     * @param requisite задаёт идентификатор счета
+     * @param balance задаёт кол-во денег на счете
+     */
 
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет получить идентификатор счёта
+     * @return возвращает идентификатор счёта
+     */
+
     public String getRequisite() {
         return requisite;
     }
+
+    /**
+     * Метод позволяет изменить идентификатор счёта
+     * @param requisite - задаёт новый идентификатор для счёта
+     */
 
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод позволяет получить баланс счёта
+     * @return возвращает баланс на счёте
+     */
+
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод позволяет изменять баланс на счёте
+     * @param balance - новое состояние баланса счёта
+     */
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    /**
+     * Метод переопределяет equals, сравнивает текущий аккаунт с
+     * аргументом метода. Если класс объекта задаваемого в качестве
+     * аргумента метода совпадает с классом аккаунта, то проверяется
+     * поле requisite этих аккаунтов. Если поля равны - возвращает true,
+     * иначе возвращает false.
+     * @param o получаемый для сравнения объект
+     * @return возвращаемый результат сравнения принятого объекта с текущим
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -38,6 +83,11 @@ public class Account {
         Account account = (Account) o;
         return Objects.equals(requisite, account.requisite);
     }
+
+    /**
+     * Метод вычисляет хеш-значение поля requisite
+     * @return возвращает хеш-значение поля requisite
+     */
 
     @Override
     public int hashCode() {
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,30 +2,75 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель клиента банка
+ * @author Igor Shvachko
+ * @version 1.0
+ */
+
 public class User {
+    /**
+     * Данные паспорта (passport) и имя (username) клиента
+     */
     private String passport;
     private String username;
+
+    /**
+     * Создание нового объекта класса конструктором
+     * @param passport данные паспорта
+     * @param username имя клиента
+     */
 
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод позволяет получить данные поля паспорта клиента
+     * @return возвращает данные поля паспорта клиента
+     */
+
     public String getPassport() {
         return passport;
     }
+
+    /**
+     * Метод позволяет изменить значение поля паспорта клиента
+     * @param passport принимает новое значение паспорта клиента
+     */
 
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод позволяет получить данные поля имени клиента
+     * @return возвращает данные поля имени клиента
+     */
+
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод позволяет изменить значение поля имени клиента
+     * @param username принимает новое значение имени клиента
+     */
+
     public void setUsername(String username) {
         this.username = username;
     }
+
+    /**
+     * Метод переопределяет equals, сравнивает текущего пользователя и
+     * пользователя задаваемого в качестве аргумента метода.
+     * Если класс объекта подаваемого в качестве аргумента метода совпадает
+     * с классом пользователя, то проверяется поле passport этих аккаунтов.
+     * Если поля равны - возвращает true, иначе возвращает false.
+     * @param o получаемый для сравнения объект
+     * @return возвращаемый результат сравнения принятого объекта с текущим
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -38,6 +83,11 @@ public class User {
         User user = (User) o;
         return Objects.equals(passport, user.passport);
     }
+
+    /**
+     * Метод вычисляет хеш-значение поля requisite
+     * @return возвращает хеш-значение поля requisite
+     */
 
     @Override
     public int hashCode() {
