2f2bc0f82907b557682f11a3b83f19d8aec2e670
igorshvachko81 <ussr981@gmail.com>
2022-06-07 00:30:37 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,38 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Address {
+    private String city;
+    private String street;
+    private int house;
+    private int apartment;
+
+    public Address(String city, String street, int house, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.house = house;
+        this.apartment = apartment;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, house, apartment);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return house == address.house
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+}
